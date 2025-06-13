4056650f74a4df110e99cbaa424fc8b93c09f9b3
Not Kobz <moturiryanonkoba@gmail.com>
2025-06-13 11:36:28 +0300

    Testing

--- fundi-frontend/src/pages/home.jsx
+++ fundi-frontend/src/pages/home.jsx
@@ -4,7 +4,7 @@ function homePage() {
 
   return (
     <div className="p-4">
-      <h2 className="text-xl font-bold mb-2">Users</h2>
+      <h2 className="text-xl font-bold mb-2">Users1</h2>
       <ul className="list-disc list-inside">
         {users.map((user) => (
           <li key={user.id}>{user.name}={user.email}</li>
